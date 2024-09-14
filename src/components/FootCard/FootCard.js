62d259cabb0bc37dca426d2721e1ca7fb5396355
Maheshwarigadakh <maheshwarigadakh2003@gmail.com>
2024-09-14 05:47:18 +0530

    button add

--- src/components/FootCard/FootCard.js
+++ src/components/FootCard/FootCard.js
@@ -4,7 +4,7 @@ import VegIcon from "./icons8-veg-48.png"
 import nonvegIcon from "./icons8-non-veg-48.png"
 
 
-function FootCard({imgUrl,title,description,isveg}) {
+function FootCard({imgUrl,title,description,isveg,price}) {
   
   return (
     <div className='foot-card'>
@@ -14,6 +14,14 @@ function FootCard({imgUrl,title,description,isveg}) {
         </h1>
       <p>{description}</p>
       <img src={isveg ? VegIcon : nonvegIcon} className='FC-icon'/>
+      {
+      price ?
+      <p className='FC-price'>
+        Rs {price}
+      </p>
+      : " Not Available"
+}
+<button className='FC-btn'>Add to card</button>
     </div>
   )
 }
